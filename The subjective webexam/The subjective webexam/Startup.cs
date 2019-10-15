c1b402f30ea9d5b069d7250bdac2a17ae092dd15
abdulrauf1996 <r03328590803@gmail.com>
2019-10-15 11:48:07 +0500

    first commit

---
+++ The subjective webexam/The subjective webexam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(The_subjective_webexam.Startup))]
+namespace The_subjective_webexam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
